608c4b99e252b652db207616a3b0bc9f285bd1c5
meowoodie <woodielove@163.com>
2015-11-17 11:27:10 +0800

    fix some bugs

--- word_url_relevance_q/script/mapper.get_word_url.py
+++ word_url_relevance_q/script/mapper.get_word_url.py
@@ -4,12 +4,14 @@ import url_util
 for line in sys.stdin:
     val = line.strip().split("\t")
 	level    = val[0]
-	bidword  = url_util.regularize_url(val[6])
-	pc_url   = url_util.regularize_url(val[13])
-	wise_url = url_util.regularize_url(val[15])
 	
 	if level == "4":
+		bidword  = url_util.regularize_url(val[6])
+		pc_url   = val[13]
+		wise_url = val[15]
 		if pc_url != "DFT":
+			pc_url   = url_util.regularize_url(pc_url)
 			print "%s\t%s" % (bidword, pc_url)
 		if wise_url != "DFT":
+			wise_url = url_util.regularize_url(wise_url)
 			print "%s\t%s" % (bidword, wise_url)
--- word_url_relevance_q/script/mapper.get_word_url_winfoid.py
+++ word_url_relevance_q/script/mapper.get_word_url_winfoid.py
@@ -6,13 +6,15 @@ SOURCE = "word_url_winfoid"
 for line in sys.stdin:
     val = line.strip().split("\t")
 	level    = val[0]
-	winfoid  = val[1]
-	bidword  = url_util.regularize_url(val[6])
-	pc_url   = url_util.regularize_url(val[13])
-	wise_url = url_util.regularize_url(val[15])
 	
 	if level == "4":
+		winfoid  = val[1]
+		bidword  = url_util.regularize_str(val[6])
+		pc_url   = val[13]
+		wise_url = val[15]
 		if pc_url != "DFT":
+			pc_url   = url_util.regularize_url(pc_url)
 			print "%s\t%s\t%s\t%s" % (bidword, pc_url, SOURCE, winfoid)
 		if wise_url != "DFT":
+			wise_url = url_util.regularize_url(wise_url)
 			print "%s\t%s\t%s\t%s" % (bidword, wise_url, SOURCE, winfoid)
